3028dc18905fa6e44cce9be13f23fbd0fca562b9
nim73 <nim730@nlobby.com>
2024-09-24 00:21:56 +0900

    feat: Quasar 개발환경 세팅

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import { Quasar } from 'quasar'
 import quasarLang from 'quasar/lang/ko-KR'
 
-// import './style.css'
 import App from './App.vue'
 
 const app = createApp(App)
