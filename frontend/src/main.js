b9da0e6491a481521f9c4705dc64d81fdf0df5df
nim73 <nim730@nlobby.com>
2024-09-23 21:59:33 +0900

    feat: vuetify 개발환경 세팅

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,18 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue'
-import router from './router/index.js'
-// import PrimeVue from 'primevue/config';
+import router from './router/index.js';
 import createVuetify  from "./plugins/vuetify.ts";
 
-import Aura from '@primevue/themes/aura';
 
 const app = createApp(App);
 
 app.use(router)
 app.use(createVuetify)
-// app.use(PrimeVue, {
-//   theme: {
-//     preset: Aura,
-//   },
-// });
+
 app.mount('#app');
